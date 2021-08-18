60f638d2a62021a77ddc195859f3140672e06c53
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-18 17:39:01 +0100

    commit tree 4e3257b91357da5cd15ec3138fa4d4bd621711d3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// function superbowlWin(record) {
+//         for (const winner of record) {
+//             if (winner.result === "W"){
+//             return winner.year}
+//         }
+// //}
 
+function superbowlWin(recordsArray){
+    const winningResults = recordsArray.find(recordObj => recordObj.result === "W")
+    return !!winningResults ? winningResults.year : undefined
+}
